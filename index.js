a3f29da33a338e0f1ed95beab0fecff404949e27
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2015-02-13 23:01:12 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var through = require('through2');
+var util = require("util");
+var path = require("path");
+var compiler = require('ember/ember-template-compiler');
+
+module.exports = function (b, opts) {
+  if (['.hbs'].indexOf(path.extname(b)) === -1) {
+    return through();
+  }
+
+  var input = '';
+  function read(data, encoding, callback) {
+    input += data;
+    callback();
+  }
+
+  function end() {
+    this.push(util.format("module.exports = Ember.Handlebars.template(%s);\n",
+      compiler.precompile(input, false)));
+    this.push(null);
+  }
+
+  return through(read, end);
+};
